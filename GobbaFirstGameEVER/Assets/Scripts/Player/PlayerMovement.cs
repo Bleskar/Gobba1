0ff8214d957e110f297c39e8e5113833f6d34623
Bleskar <85394776+Bleskar@users.noreply.github.com>
2022-08-28 18:07:57 +0200

    Player movement script (och tog bort filips dumma script)
    spelar script med acceleration och retardation
    
    tog bort filip Rs onödiga script (URL till image)

---
+++ GobbaFirstGameEVER/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Options")]
+    [SerializeField] float acceleration = 50f;
+    [SerializeField] float deacceleration = 50f;
+    [SerializeField] float topSpeed = 8f;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    public void Movement(float x, float y) => Movement(new Vector2(x, y));
+    public void Movement(Vector2 input)
+    {
+        input.Normalize();
+
+        if (input != Vector2.zero)
+        {
+            //moving
+            rb.velocity += input * Time.deltaTime * acceleration;
+            if (rb.velocity.magnitude > topSpeed)
+                rb.velocity = rb.velocity.normalized * topSpeed;
+            return;
+        }
+        //breaking
+        Vector2 direction = rb.velocity.normalized;
+        rb.velocity -= direction * Time.deltaTime * deacceleration;
+
+        if (rb.velocity.normalized != direction)
+            rb.velocity = Vector2.zero;
+    }
+}
